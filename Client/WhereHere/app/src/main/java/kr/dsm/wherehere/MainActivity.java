ce53cef8e6a2ffcc430816400b39d1e0fbc31bbf
hojak99 <hojak99@gmail.com>
2017-03-31 18:48:20 +0900

    Create RangkingFragment class

--- Client/WhereHere/app/src/main/java/kr/dsm/wherehere/MainActivity.java
+++ Client/WhereHere/app/src/main/java/kr/dsm/wherehere/MainActivity.java
@@ -3,6 +3,9 @@ package kr.dsm.wherehere;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,34 +13,42 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private RankingFragment rankingFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            fragmentTransaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragmentTransaction.replace(R.id.content, rankingFragment).commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragmentTransaction.replace(R.id.content, rankingFragment).commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragmentTransaction.replace(R.id.content, rankingFragment).commit();
                     return true;
             }
             return false;
         }
-
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fragmentManager = getSupportFragmentManager();
+
+        rankingFragment = new RankingFragment();
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        fragmentManager.beginTransaction().replace(R.id.content, new RankingFragment()).commit();
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
